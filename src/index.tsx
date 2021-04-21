6145b7ef626b3a96d53fcfcc010001f85ea43bba
Thomas Ponthoreau <tponthoreau@MacBook-Pro-de-Thomas.local>
2021-04-21 15:49:59 +0200

    graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql/",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
